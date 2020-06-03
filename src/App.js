2a6267acc2cf0eb046b7136294c2f84b2fa23220
JPReynolds <jordanreynolds0611@gmail.com>
2020-06-03 20:17:56 +0100

    dealDealerCards function

--- src/App.js
+++ src/App.js
@@ -96,15 +96,18 @@ class App extends React.Component {
     const selectedCardTwo = cards[Math.floor(Math.random() * cards.length)];
     const dealerCardOne = cards[Math.floor(Math.random() * cards.length)];
     const dealerCardTwo = cards[Math.floor(Math.random() * cards.length)];
+
     if (this.state.playersHand.length >= 2) {
       const newPlayersHand = [...this.state.playersHand, selectedCardOne];
       const dealersHand = [dealerCardOne, dealerCardTwo];
-      const score = this.state.playerScore + selectedCardOne.value;
+      const playerScore = this.state.playerScore + selectedCardOne.value;
+      const dealerScore = dealerCardOne.value + dealerCardTwo.value;
       this.setState({
         playersHand: newPlayersHand,
         cardsDealt: true,
-        playerScore: score,
+        playerScore,
         dealersHand,
+        dealerScore,
       });
     } else {
       const newPlayersHand = [
@@ -123,6 +126,56 @@ class App extends React.Component {
 
   stick = () => {
     this.setState({ stick: true });
+  };
+
+  dealCardsDealer = () => {
+    const cards = [
+      // { value: 1, suit: 'H' },
+      { value: 2, suit: 'H' },
+      { value: 3, suit: 'H' },
+      { value: 4, suit: 'H' },
+      { value: 5, suit: 'H' },
+      { value: 6, suit: 'H' },
+      { value: 7, suit: 'H' },
+      { value: 8, suit: 'H' },
+      { value: 9, suit: 'H' },
+      { value: 10, suit: 'S' },
+      // { value: 1, suit: 'S' },
+      { value: 2, suit: 'S' },
+      { value: 3, suit: 'S' },
+      { value: 4, suit: 'S' },
+      { value: 5, suit: 'S' },
+      { value: 6, suit: 'S' },
+      { value: 7, suit: 'S' },
+      { value: 8, suit: 'S' },
+      { value: 9, suit: 'S' },
+      { value: 10, suit: 'S' },
+      // { value: 1, suit: 'D' },
+      { value: 2, suit: 'D' },
+      { value: 3, suit: 'D' },
+      { value: 4, suit: 'D' },
+      { value: 5, suit: 'D' },
+      { value: 6, suit: 'D' },
+      { value: 7, suit: 'D' },
+      { value: 8, suit: 'D' },
+      { value: 9, suit: 'D' },
+      { value: 10, suit: 'D' },
+      // { value: 1, suit: 'C' },
+      { value: 2, suit: 'C' },
+      { value: 3, suit: 'C' },
+      { value: 4, suit: 'C' },
+      { value: 5, suit: 'C' },
+      { value: 6, suit: 'C' },
+      { value: 7, suit: 'C' },
+      { value: 8, suit: 'C' },
+      { value: 9, suit: 'C' },
+      { value: 10, suit: 'C' },
+    ];
+    const dealerCardOne = cards[Math.floor(Math.random() * cards.length)];
+    const dealerCardTwo = cards[Math.floor(Math.random() * cards.length)];
+
+    if (this.state.dealerScore < 17) {
+    }
   };
 }
 
